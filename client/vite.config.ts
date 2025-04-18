571c1cbd59709da8c7bb960a684a7fdbd62a0c3e
PETERBILT-PLUS <v8engine4stroke@gmail.com>
2025-04-19 01:08:17 +0200

    last one ok

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import vercel from 'vite-plugin-vercel';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss(), flowbiteReact(), vercel()],
   ssr: {
     noExternal: ["react-router-dom"]
   }
