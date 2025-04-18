43931a195e3627bb7c2bcac33694efcdab97807b
PETERBILT-PLUS <v8engine4stroke@gmail.com>
2025-04-19 00:32:53 +0200

    fourth

--- client/src/entry-server.tsx
+++ client/src/entry-server.tsx
@@ -4,14 +4,11 @@ import {
   renderToPipeableStream,
 } from 'react-dom/server'
 import App from './App'
-import { HelmetProvider } from 'react-helmet-async'
 
 export function render(_url: string, options?: RenderToPipeableStreamOptions) {
   return renderToPipeableStream(
     <StrictMode>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
     </StrictMode>,
     options,
   )
