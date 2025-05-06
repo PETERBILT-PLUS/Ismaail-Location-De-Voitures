1b17c8b13668ef3e9ee01d1754f0adfb7dfbb77c
PETERBILT-PLUS <v8engine4stroke@gmail.com>
2025-05-06 20:39:57 +0200

    fix all this again again a

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -65,8 +65,8 @@ function Home() {
     return (
         <main>
             <Helmet>
-                <title>Location de Voitures Ouarzazate Marrakech</title>
-                <meta name="description" content="Location de voitures à l'aéroport de Marrakech et Ouarzazate – Louez une voiture pas chère en ligne. Choisissez votre véhicule et réservez facilement en quelques clics." />
+                <title>Louer une voiture à Ouarzazate | Galaxy Cars</title>
+                <meta name="description" content="Location de voitures économiques à Ouarzazate. Réservez en ligne avec Galaxy Cars – Livraison gratuite à l’aéroport en été !" />
                 <meta name="keywords" content="location voiture Marrakech, louer voiture Marrakech, voiture pas cher Marrakech, location auto Marrakech, Marrakech car rental, location voiture Marrakech, louer voiture Ouarzazate, voiture pas cher Ouarzazate, location auto Ouarzazate, Ouarzazate car rental" />
                 <link rel="canonical" href="https://galaxycars2005.com" />
                 <meta property="og:locale:alternate" content="en_US" />
